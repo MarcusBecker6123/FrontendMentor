c479b1a968a301ed6f0a09e9e523a4e48bc93b0d
MarcusBecker6123 <marcusbecker1990@icloud.com>
2025-08-08 00:02:58 +0200

    added a script when clicking on the card, it have a link to my solutions page, also added css hover effekt with box shadow and scale for my card element

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("qr-code").addEventListener("click", () => {
+    window.open('https://www.frontendmentor.io/solutions/little-card-box-using-flex-with-propper-roundings-padding-and-margin-djeoj86ooK', '_blank');
+});
